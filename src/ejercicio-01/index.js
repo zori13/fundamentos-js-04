5781047e2408eae0c028e684e30eb0812c7c4393
alexmf91 <alexmunoz91@gmail.com>
2023-11-01 20:42:09 +0100

    Añadir ejercicio-01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,9 @@
 // Escribe tu código aquí:
+const person = {
+  name: 'John',
+  age: 30,
+  city: 'New York'
+}
+
+console.log('Notacion de puntos', person.name)
+console.log('Notacion de corchetes', person['name'])
