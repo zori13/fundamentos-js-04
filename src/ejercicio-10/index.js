e4384e9ba66d9fd237cbd0bce5e8ffd98e9f499f
zori13 <zori.gon@mail.com>
2023-11-18 19:43:07 +0100

    Ejercicio 10

--- src/ejercicio-10/index.js
+++ src/ejercicio-10/index.js
@@ -1,1 +1,23 @@
-// Escribe tu código aquí:
+class Person{
+    constructor(nombre,edad){
+        this.nombre=nombre
+        this.edad=edad
+    }
+    introduce(){
+        console.log(`Hola, soy ${this.nombre} y tengo ${this.edad} años.`)
+    }
+}
+class Student extends Person{
+    constructor(nombre,edad,grado){
+        super(nombre,edad)
+        this.grado=grado
+    }
+  introduce(){
+    console.log(`Hola, soy ${this.nombre} y tengo ${this.edad} años y estoy en el grado ${this.grado}.`)
+ }
+}
+
+const persona=new Person('Andrés',37)
+persona.introduce()
+const student=new Student('Carla', 25,3)
+student.introduce()
