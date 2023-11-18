ef41aa0a7770819301a7e01b3103420f9316f420
zori13 <zori.gon@mail.com>
2023-11-18 11:21:44 +0100

    Ejercicio 5

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,14 @@
-// Escribe tu código aquí:
+class Rectangulo{
+    constructor(ancho,alto){
+        this.ancho=ancho
+        this.alto=alto
+
+    }
+
+calcularArea(){
+    return this.ancho*this.alto 
+ }
+}
+
+const rectangulo=new Rectangulo(5,10)
+console.log('Area:', rectangulo.calcularArea())
