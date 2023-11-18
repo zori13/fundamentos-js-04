dc22446fbe934fd6664a674e9721837e1c3f584a
zori13 <zori.gon@mail.com>
2023-11-18 18:44:02 +0100

    Ejercicio 6

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,21 @@
-// Escribe tu código aquí:
+class Rectangulo{
+    constructor(ancho,alto){
+        this.ancho=ancho
+        this.alto=alto
+
+    }
+
+calcularArea(){
+    return this.ancho*this.alto 
+ }
+}
+class Square extends Rectangulo{
+    constructor(sideLength){
+        super(sideLength,sideLength)
+    }
+}
+const rectangulo=new Rectangulo(5,10)
+const square =new Square(8)
+console.log('Area:', rectangulo.calcularArea())
+console.log('Área del cuadrado:', square.calcularArea() )
+
